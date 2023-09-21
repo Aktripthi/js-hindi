038ac17cb4125346433939f6922c6b3d309b66b6
Aktripthi <139927426+Aktripthi@users.noreply.github.com>
2023-09-21 12:52:07 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId =288889
+let accountEmail = "amit02@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+
+accountEmail = "hc@hc.com"
+accountPassword ="1333"
+accountCity="meeruth"
+ 
+// accountId =2 //              not allowed
+console.table([accountEmail,accountId,accountPassword,accountCity]);
