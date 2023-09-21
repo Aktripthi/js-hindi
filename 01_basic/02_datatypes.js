bbf9b95c58e5f4ba6e5678cdb5f365d9d9d7e2ca
Aktripthi <139927426+Aktripthi@users.noreply.github.com>
2023-09-21 17:49:44 +0000

    datatypes done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+                        
+ let name = "Amit"
+ let age = 18
+ let isLoggedIn = false
+ let state;                       
+                        
+                        
+                        
+                        //primitive data type 
+//number => 
+//biginit
+//string =>""
+//boolean => true/false
+//null => stand alone value
+//undefined =>
+//symbol => unique
+                                //non Primitive data type
+  
+console.log(typeof undefined);     //undefined
+console.log(typeof null);          // object
+                          
